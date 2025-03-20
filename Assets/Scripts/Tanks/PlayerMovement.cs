958be50e7c377cedd2d7a3992780619a9f1c9979
Cyril Pitar <98352804+AyRron@users.noreply.github.com>
2025-03-20 13:43:03 +0100

    Add Camera

---
+++ Assets/Scripts/Tanks/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
