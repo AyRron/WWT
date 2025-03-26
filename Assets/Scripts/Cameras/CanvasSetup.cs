b87d11e6abd5314d1e3230eb2761de5646eae09d
Cyril Pitar <98352804+AyRron@users.noreply.github.com>
2025-03-26 10:25:33 +0100

    Update main scene

---
+++ Assets/Scripts/Cameras/CanvasSetup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CanvasSetup : MonoBehaviour
+{
+    void Start()
+    {
+        Canvas canvas = GetComponent<Canvas>();
+        if (canvas != null)
+        {
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        }
+
+        if (FindObjectOfType<EventSystem>() == null)
+        {
+            GameObject eventSystem = new GameObject("EventSystem");
+            eventSystem.AddComponent<EventSystem>();
+            eventSystem.AddComponent<StandaloneInputModule>();
+        }
+    }
+}
