b3f8b8d2b0961ddc9b77e217401fee1eebbfab75
DupLeo <109819277+DupLeo@users.noreply.github.com>
2025-04-08 12:13:20 +0200

    fix bug move

--- Assets/tankIdleState.cs
+++ Assets/tankIdleState.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class tankIdleState : StateMachineBehaviour
 {
     AttackController attackController;
+
+    public float followingDistance = 10f;
+
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -14,10 +17,16 @@ public class tankIdleState : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         // Check if there is an available target
-        if (attackController.targetToAttack != null) { 
+        if (attackController.targetToAttack != null) {
 
-            // transition to Follow State
-            animator.SetBool("isFollowing", true);
+            float distanceFromTarget = Vector3.Distance(attackController.targetToAttack.position, animator.transform.position);
+
+
+            if (followingDistance < distanceFromTarget)
+            {
+                animator.SetBool("isFollowing", true);
+            }
+
         }
 
     }
