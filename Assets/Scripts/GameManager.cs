4a4914b352e112ef0b11f83c0a0def94f2b7e301
Guillaume Rey <grey02@orange.fr>
2025-04-08 16:38:24 +0200

    menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<Tank> tanksAllies = new List<Tank>();
+    public List<Tank> tanksEnemies = new List<Tank>();
+
+    public Image alliesScore;
+    public Image enemiesScore;
+
+    public float scoreAllies;
+    public float scoreEnemies;
+
+    public GameObject timerUI;
+    private TextMeshProUGUI _timerText;
+    private float _timer = 12f;
+    
+    private bool _isPlaying;
+    
+    public GameObject mainMenuUI;
+    
+    private void Awake()
+    {
+        if (timerUI != null)
+        {
+            _timerText = timerUI.GetComponent<TextMeshProUGUI>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScoreBare();
+        if (_isPlaying)
+        {
+            UpdateTimer();
+        }
+    }
+
+    private void UpdateScoreBare()
+    {
+        alliesScore.fillAmount = scoreAllies / 100f;
+        enemiesScore.fillAmount = scoreEnemies / 100f;
+    }
+    
+    private void UpdateTimer()
+    {
+        if (_timer > 0)
+        {
+            _timer -= Time.deltaTime;
+            var minutes = Mathf.FloorToInt(_timer / 60);
+            var seconds = Mathf.FloorToInt(_timer % 60);
+            _timerText.text = $"{minutes:D2}:{seconds:D2}";
+        }
+        else
+        {
+            _timer = 0;
+            _timerText.text = "00:00";
+            EndGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        _isPlaying = true;
+        mainMenuUI.SetActive(false);
+    }
+    
+    private void EndGame()
+    {
+        _isPlaying = false;
+        mainMenuUI.SetActive(true);
+    }
+}
